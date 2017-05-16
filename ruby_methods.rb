091990a040327f3d6bcdfe0f6382c2248560483b
heyjulieh <heyjulieh@gmail.com>
2017-05-15 19:31:27 -0700

    finished number 3

--- ruby_methods.rb
+++ ruby_methods.rb
@@ -32,3 +32,14 @@ letter_count("apple")
 
 def mock_me (input)
   puts "Please input some words"
+  input = gets.chomp
+    while input == input
+      puts input
+      system("stty raw -echo")
+        char = STDIN.read_nonblock(1) rescue nil
+          system("stty -raw echo")
+            break if /q/i =~ char
+              sleep(2)
+    end
+end
+mock_me ("input")
